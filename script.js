0dc2730d59c1c4fdca56897e2e8cee8418449f5b
Nick Ford <nfordhk@gmail.com>
2020-10-05 21:14:47 -0700

    building skeleton for new project

---
+++ script.js
@@ -0,0 +1,25 @@
+/****
+ * 
+ * Declare Vars
+ * 
+ **/
+
+//All of your book objects are going to be stored in a simple array
+//Add a function to the script (not the constructor) that can take user’s input
+//Store the new book objects into an array. 
+
+let myLibrary = [];
+
+/****
+ * 
+ * Book Functions
+ * 
+ ***/
+
+function Book() {
+  // the constructor.
+}
+
+function addBookToLibrary() {
+  // code here
+}
